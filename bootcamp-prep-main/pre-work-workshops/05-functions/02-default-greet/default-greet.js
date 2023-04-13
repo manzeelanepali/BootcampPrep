786be695ba0452e463e4cb77358d68d6f873448a
manzeelanepali <nepalimanzeela@gmail.com>
2023-04-13 12:07:33 +0545

    test case simple greeet

--- bootcamp-prep-main/pre-work-workshops/05-functions/02-default-greet/default-greet.js
+++ bootcamp-prep-main/pre-work-workshops/05-functions/02-default-greet/default-greet.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+function defaultGreet(first, lastname) {
+  if (first && lastname) {
+    return `Hi ${first} ${lastname}!`;
+  } else {
+    return `Hi ${first} Doe!`;
+  }
+}
+
+console.log(defaultGreet(Natalie, Cole));
+// console.log(defaultGreet(john));
