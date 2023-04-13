5b4d5b3ea1b4201d790879b36393e46da18d965a
manzeelanepali <nepalimanzeela@gmail.com>
2023-04-13 11:04:22 +0545

    completed two tests

--- bootcamp-prep-main/pre-work-workshops/04-control-flow/01-founder-finder/founder-finder.js
+++ bootcamp-prep-main/pre-work-workshops/04-control-flow/01-founder-finder/founder-finder.js
@@ -1,7 +1,13 @@
-let name = 'David';
+let name = "David";
 // let name = 'Nimit';
 // let name = 'Someone else';
 
 let found;
 
 // YOUR CODE BELOW
+
+if (name === "David" || "Nimit") {
+  found = true;
+} else {
+  found = false;
+}
