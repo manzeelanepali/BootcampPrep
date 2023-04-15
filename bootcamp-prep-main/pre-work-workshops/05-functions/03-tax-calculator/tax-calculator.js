9ff3939039087b44d960afd0af1cd655e4308399
manzeelanepali <nepalimanzeela@gmail.com>
2023-04-15 21:34:34 +0545

    test case of tax rate

--- bootcamp-prep-main/pre-work-workshops/05-functions/03-tax-calculator/tax-calculator.js
+++ bootcamp-prep-main/pre-work-workshops/05-functions/03-tax-calculator/tax-calculator.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+
+function taxCalculator(price, state) {
+  let taxRate = 0;
+  if (state === "NY") {
+    taxRate = 0.04;
+  } else if (state === "NJ") {
+    taxRate = 0.06625;
+  }
+  const afterTax = price + price * taxRate;
+  return afterTax;
+}
