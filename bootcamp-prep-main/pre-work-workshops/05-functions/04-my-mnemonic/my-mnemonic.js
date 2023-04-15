2043d78949c684ffa51e5250428cc16f2427bce8
manzeelanepali <nepalimanzeela@gmail.com>
2023-04-15 21:47:38 +0545

    test case for the mnemonic

--- bootcamp-prep-main/pre-work-workshops/05-functions/04-my-mnemonic/my-mnemonic.js
+++ bootcamp-prep-main/pre-work-workshops/05-functions/04-my-mnemonic/my-mnemonic.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+function myMnemonic(...strings) {
+  let mnemonic = "";
+  for (let i = 0; i < strings.length && mnemonic.length < 4; i++) {
+    mnemonic = mnemonic + strings[i][0];
+  }
+  return mnemonic.toUpperCase();
+}
+
+console.log(myMnemonic("Must", "Accept", "Sugar", "Happiness"));
