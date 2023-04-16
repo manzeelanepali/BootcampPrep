b8b514a1cfdc2f1e6c8017bb562164607295183e
manzeelanepali <nepalimanzeela@gmail.com>
2023-04-16 10:12:46 +0545

    test for finding the maxium number in three arguments

--- bootcamp-prep-main/pre-work-workshops/06-problem-set/04-max-of-three/max-of-three.js
+++ bootcamp-prep-main/pre-work-workshops/06-problem-set/04-max-of-three/max-of-three.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+
+function maxOfThree(one, two, three) {
+  if (one > two && one > three) {
+    return one;
+  } else if (two > one && two > three) {
+    return two;
+  } else {
+    return three;
+  }
+}
+console.log(maxOfThree(10, 12, 14));
