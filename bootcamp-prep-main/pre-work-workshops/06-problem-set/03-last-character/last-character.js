de518f392d1b2d3f2669bde74d91284eede9bc44
manzeelanepali <nepalimanzeela@gmail.com>
2023-04-16 10:07:57 +0545

    excessing the second last element in the string and comparing

--- bootcamp-prep-main/pre-work-workshops/06-problem-set/03-last-character/last-character.js
+++ bootcamp-prep-main/pre-work-workshops/06-problem-set/03-last-character/last-character.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+function lastCharacter(str1, str2) {
+  if (str1.slice(-1) === str2.slice(-1)) {
+    // slice(-1 for acessing the last element in the string and string(-2 for acessing the second last elemen in the string))
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(lastCharacter("Rams", "sams"));
