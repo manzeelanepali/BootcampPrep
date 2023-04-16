c19ef9fbad98523dd418696ad66a2f237ce23a22
manzeelanepali <nepalimanzeela@gmail.com>
2023-04-16 10:33:27 +0545

    completed test cases of 6

--- bootcamp-prep-main/pre-work-workshops/06-problem-set/05-every-which-way/every-which-way.js
+++ bootcamp-prep-main/pre-work-workshops/06-problem-set/05-every-which-way/every-which-way.js
@@ -1,1 +1,31 @@
-// YOUR CODE BELOW
+// // YOUR CODE BELOW
+// function everyWhichWay(one, two, three) {
+//   if (one + two === three) {
+//     return sum;
+//   } else if (one - two === three) {
+//     return difference;
+//   } else if (one * two === three) {
+//     return product;
+//   } else if (one / two === three) {
+//     return fraction;
+//   } else {
+//     return null;
+//   }
+// }
+// console.log(everyWhichWay(10, 20, 30));
+
+function everyWhichWay(one, two, three) {
+  if (one + two === three) {
+    return "sum";
+  } else if (one - two === three) {
+    return "difference";
+  } else if (one * two === three) {
+    return "product";
+  } else if (one / two === three) {
+    return "fraction";
+  } else {
+    return null;
+  }
+}
+
+console.log(everyWhichWay(10, 10, 20));
