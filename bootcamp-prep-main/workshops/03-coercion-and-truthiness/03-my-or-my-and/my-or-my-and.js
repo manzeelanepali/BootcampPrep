40721291e9229605e7955276655f8d12d0dfdaee
manzeelanepali <nepalimanzeela@gmail.com>
2023-04-18 21:30:37 +0545

    feat : test cases to ignore the non zero element and makes a new variable with non zero element only

--- bootcamp-prep-main/workshops/03-coercion-and-truthiness/03-my-or-my-and/my-or-my-and.js
+++ bootcamp-prep-main/workshops/03-coercion-and-truthiness/03-my-or-my-and/my-or-my-and.js
@@ -1,1 +1,8 @@
 // YOUR CODE BELOW
+function myOr(arg1, arg2, arg3) {
+  if ((arg1 && arg2 === true) || (arg2 && arg3 == true)) {
+    return true;
+  }
+}
+
+function myAnd(a, b, c) {}
--- bootcamp-prep-main/workshops/03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
+++ bootcamp-prep-main/workshops/03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+function zeroDarkThirty(number) {
+  if (number == 0) {
+    return NaN;
+  }
+  let newStr = number.toString();
+  let finalRes = "";
+  for (let i = 0; i < newStr.length; i++) {
+    if (newStr[i] !== "0") {
+      finalRes += newStr[i];
+    }
+  }
+  let convertToNum = Number(finalRes);
+  return convertToNum;
+}
