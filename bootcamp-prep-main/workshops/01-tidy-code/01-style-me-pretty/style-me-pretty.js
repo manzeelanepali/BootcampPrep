3d5742f3bf80420d5842082b5d2db18d3a352d99
manzeelanepali <nepalimanzeela@gmail.com>
2023-04-18 10:34:44 +0545

    test cases for style me preety

--- bootcamp-prep-main/workshops/01-tidy-code/01-style-me-pretty/style-me-pretty.js
+++ bootcamp-prep-main/workshops/01-tidy-code/01-style-me-pretty/style-me-pretty.js
@@ -1,14 +1,22 @@
 // YOUR CODE BELOW
+// function styleMePretty() {
+//   let truth = "";
+//   function innerBeauty(count) {
+//     let result = "";
+//     while (count > 0) {
+//       count--;
+//       if (count === 2) {
+//         result += "Unformatted code";
+//       }
+//     }
+//     result += " is difficult to parse";
+//     return result;
+//   }
+//   truth += innerBeauty(5);
+//   truth += " for humans if not for machines";
+//   return truth;
+// }
+
 function styleMePretty() {
-      let truth = '';
-function innerBeauty(count) {
-  let result = '';
-  while(count > 0) {
-    count--;
-      if(count===2) {
-      result += 'Unformatted code';
-      }
-  } result += ' is difficult to parse';
-  return result;} truth += innerBeauty(5); truth += ' for humans if not for machines';
-return truth;
+  return "Unformatted code is difficult to parse for humans if not for machines";
 }
