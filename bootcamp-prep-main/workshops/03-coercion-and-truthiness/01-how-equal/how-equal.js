a51579a6a75ec3f5a2a3f4797b667d296cd45258
manzeelanepali <nepalimanzeela@gmail.com>
2023-04-18 14:39:39 +0545

    coercion and relation test 1st

--- bootcamp-prep-main/workshops/03-coercion-and-truthiness/01-how-equal/how-equal.js
+++ bootcamp-prep-main/workshops/03-coercion-and-truthiness/01-how-equal/how-equal.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+function howEqual(one, two) {
+  if (one === two) {
+    return "strictly";
+  } else if (one == two) {
+    return "loosely";
+  } else {
+    return "not equal";
+  }
+}
