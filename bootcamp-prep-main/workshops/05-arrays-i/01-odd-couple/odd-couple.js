5839f4cbdc3a333c793730da2bd84d019858e9e4
manzeelanepali <nepalimanzeela@gmail.com>
2023-04-19 11:05:14 +0545

    finding the sum of first two odd numbers in an array

--- bootcamp-prep-main/workshops/05-arrays-i/01-odd-couple/odd-couple.js
+++ bootcamp-prep-main/workshops/05-arrays-i/01-odd-couple/odd-couple.js
@@ -1,6 +1,13 @@
 // YOUR CODE BELOW
 
 function oddCouple(arr) {
-  const odds = arr.filter((num) => num % 2 !== 0);
-  return odds.slice(0, 2);
+  let res = [];
+  for (let i = 0; i < arr.length; i++) {
+    let curVal = arr[i];
+    if (curVal % 2 !== 0) {
+      res.push(curVal);
+    }
+  }
+  let finalRes = res.slice(0, 2);
+  return finalRes;
 }
