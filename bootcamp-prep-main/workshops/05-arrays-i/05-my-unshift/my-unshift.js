a7f59b76538fcdff4f631c4c44e5c4b5cd2cae7f
manzeelanepali <nepalimanzeela@gmail.com>
2023-04-19 12:39:34 +0545

    arry 11 ... first test

--- bootcamp-prep-main/workshops/05-arrays-i/05-my-unshift/my-unshift.js
+++ bootcamp-prep-main/workshops/05-arrays-i/05-my-unshift/my-unshift.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+
+//  unshift built-in method pushes the value in front
+
+function myUnshift(arr, value) {
+  const newArr = [value];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(arr[i]);
+  }
+  return newArr;
+}
--- bootcamp-prep-main/workshops/06-arrays-ii/01-even-and-odd/even-and-odd.js
+++ bootcamp-prep-main/workshops/06-arrays-ii/01-even-and-odd/even-and-odd.js
@@ -1,1 +1,17 @@
 // YOUR CODE BELOW
+
+function evenAndOdd(array) {
+  let oddNum = [];
+  let evenNum = [];
+
+  let finalRes = [evenNum, oddNum];
+  for (let i = 0; i < array.length; i++) {
+    let currVal = array[i];
+    if (currVal % 2 === 0) {
+      evenNum.push(currVal);
+    } else {
+      oddNum.push(currVal);
+    }
+  }
+  return finalRes;
+}
