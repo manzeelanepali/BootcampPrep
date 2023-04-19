f7beea2f49369fcf4762c0252adba06914e2b46b
manzeelanepali <nepalimanzeela@gmail.com>
2023-04-19 12:02:44 +0545

    feat ; reversing array without using builtin reverse method

--- bootcamp-prep-main/workshops/05-arrays-i/04-my-reverse/my-reverse.js
+++ bootcamp-prep-main/workshops/05-arrays-i/04-my-reverse/my-reverse.js
@@ -1,1 +1,9 @@
 // YOUR CODE BELOW
+function myReverse(array) {
+  let finalArr = [];
+  for (let i = array.length - 1; i >= 0; i--) {
+    let curVal = array[i];
+    finalArr.push(curVal);
+  }
+  return finalArr;
+}
