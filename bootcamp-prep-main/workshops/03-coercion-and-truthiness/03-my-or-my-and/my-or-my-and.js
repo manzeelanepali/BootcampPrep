68ae4d23e40529ad3aced9119eab3b7a47ad2f8c
manzeelanepali <nepalimanzeela@gmail.com>
2023-04-19 09:22:12 +0545

    myOr and myAnd

--- bootcamp-prep-main/workshops/03-coercion-and-truthiness/03-my-or-my-and/my-or-my-and.js
+++ bootcamp-prep-main/workshops/03-coercion-and-truthiness/03-my-or-my-and/my-or-my-and.js
@@ -11,3 +11,17 @@ function myOr(first, second, third) {
 function myAnd(one, two, three) {
   if (one && two && !three) return;
 }
+
+function myAnd(first, second, third) {
+  if (first && second && third) {
+    return third;
+  } else {
+    if (!first) {
+      return first;
+    } else if (!second) {
+      return second;
+    } else {
+      return third;
+    }
+  }
+}
--- bootcamp-prep-main/workshops/03-coercion-and-truthiness/04-only-one/only-one.js
+++ bootcamp-prep-main/workshops/03-coercion-and-truthiness/04-only-one/only-one.js
@@ -1,1 +1,20 @@
 // YOUR CODE BELOW
+// function onlyOne(one, two, three) {
+//   return Boolean(one || two || three);
+// }
+
+// function onlyOne(arg1, arg2, arg3) {
+//   return Boolean(arg1) ^ Boolean(arg2) ^ Boolean(arg3);
+// }
+
+function onlyOne(one, two, three) {
+  debugger;
+
+  if (
+    (!one && !two && three) || ///niru ko code
+    (!one && two && !three) ||
+    (one && !two && !three)
+  ) {
+    return true;
+  } else return false;
+}
