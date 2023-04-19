4d431db3fecbadc4e2dd3ea4009b6f0c40318f19
manzeelanepali <nepalimanzeela@gmail.com>
2023-04-19 11:14:57 +0545

    test second if searchVal and the array matches

--- bootcamp-prep-main/workshops/05-arrays-i/02-my-includes/my-includes.js
+++ bootcamp-prep-main/workshops/05-arrays-i/02-my-includes/my-includes.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+function myIncludes(array, searchValue) {
+  for (let i = 0; i < array.length; i++) {
+    let currVal = array[i];
+    if (currVal === searchValue) {
+      return true;
+    }
+  }
+  return false;
+}
