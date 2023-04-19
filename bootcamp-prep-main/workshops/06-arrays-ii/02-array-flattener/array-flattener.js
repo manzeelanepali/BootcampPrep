c5cb24c1bba1e0d596d26da7b059f3d0eeeeefb2
manzeelanepali <nepalimanzeela@gmail.com>
2023-04-19 22:22:02 +0545

    feat : array flattering test

--- bootcamp-prep-main/workshops/06-arrays-ii/02-array-flattener/array-flattener.js
+++ bootcamp-prep-main/workshops/06-arrays-ii/02-array-flattener/array-flattener.js
@@ -1,1 +1,17 @@
 // YOUR CODE BELOW
+function arrayFlattener(array) {
+  let newArray = [];
+  debugger;
+  for (let i = 0; i < array.length; i++) {
+    let curVal = array[i];
+    if (Array.isArray(curVal)) {
+      for (let j = 0; j < curVal.length; j++) {
+        let num = curVal[j];
+        newArray.push(num);
+      }
+    } else newArray.push(curVal);
+  }
+  return newArray;
+}
+
+console.log(arrayFlattener([1, [2, 3], 4]));
--- bootcamp-prep-main/workshops/06-arrays-ii/03-zoo-inventory/zoo-inventory.js
+++ bootcamp-prep-main/workshops/06-arrays-ii/03-zoo-inventory/zoo-inventory.js
@@ -1,1 +1,21 @@
 // YOUR CODE BELOW
+function zooInventory(array) {
+  //   function zooInventory(array) {
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+    let animal = array[i][0];
+    let type = array[i][1][0];
+    let age = array[i][1][1];
+    let str = `${animal} the ${type} is ${age}.`;
+    result.push(str);
+  }
+  return result;
+}
+
+console.log(
+  zooInventory([
+    ["King Kong", ["gorilla", 42]],
+    ["Nemo", ["fish", 5]],
+    ["Punxsutawney Phil", ["groundhog", 11]],
+  ])
+);
