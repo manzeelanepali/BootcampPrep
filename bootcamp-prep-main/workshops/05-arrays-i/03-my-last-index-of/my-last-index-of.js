e3367f3f6e55ccbddbdcf56708626a53c50f0f58
manzeelanepali <nepalimanzeela@gmail.com>
2023-04-19 11:55:15 +0545

    test : my lastIndexoF

--- bootcamp-prep-main/workshops/05-arrays-i/03-my-last-index-of/my-last-index-of.js
+++ bootcamp-prep-main/workshops/05-arrays-i/03-my-last-index-of/my-last-index-of.js
@@ -1,1 +1,8 @@
-// YOUR CODE BELOW
+function myLastIndexOf(array, searchValue, startIdx = array.length - 1) {
+  for (let i = startIdx; i >= 0; i--) {
+    if (array[i] === searchValue) {
+      return i;
+    }
+  }
+  return -1;
+}
