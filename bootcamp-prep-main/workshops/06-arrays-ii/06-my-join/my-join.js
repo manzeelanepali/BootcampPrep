f15792603e29059094278cf0e0e1c82f62d4ac9a
manzeelanepali <nepalimanzeela@gmail.com>
2023-04-20 12:14:58 +0545

    my join

--- bootcamp-prep-main/workshops/06-arrays-ii/06-my-join/my-join.js
+++ bootcamp-prep-main/workshops/06-arrays-ii/06-my-join/my-join.js
@@ -1,1 +1,22 @@
 // YOUR CODE BELOW
+function myJoin(array, separator) {
+  debugger;
+  separator = separator || ",";
+  let string = "";
+
+  for (let i = 0; i < array.length; i++) {
+    let curVal = array[i];
+    if (curVal === null || curVal === undefined) {
+      curVal = "";
+    }
+    string = string + curVal;
+    //string = string + curVal + separator;
+
+    if (i !== array.length - 1) {
+      /// this logic add separtor in the string which is not equal to last index
+      string = string + separator;
+    }
+  }
+  //string = string.slice(0, -1);
+  return string;
+}
