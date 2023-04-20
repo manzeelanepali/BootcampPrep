ac710b91615ea40615d2532944a966e4807439ce
manzeelanepali <nepalimanzeela@gmail.com>
2023-04-20 12:16:57 +0545

    test : rotate array

--- bootcamp-prep-main/workshops/06-arrays-ii/08-rotate-array/rotate-array.js
+++ bootcamp-prep-main/workshops/06-arrays-ii/08-rotate-array/rotate-array.js
@@ -1,1 +1,21 @@
 // YOUR CODE BELOW
+function rotateArray(originalArray, rotateNum) {
+  debugger;
+  //let newArray = [];
+  const copyArray = originalArray.slice(); // create a copy of the original array
+
+  if (rotateNum > 0) {
+    // rotate to the right
+    for (let i = 0; i < rotateNum; i++) {
+      //pop method sliced last element from the copyArray
+      copyArray.unshift(copyArray.pop()); //unshift add the last sliced element to the front  in the copyArray
+    }
+  } else if (rotateNum < 0) {
+    // rotate to the left
+    for (let i = 0; i < Math.abs(rotateNum); i++) {
+      copyArray.push(copyArray.shift()); //shift method remove the element from the zero index and push add that removed element from behind
+    }
+  }
+
+  return copyArray;
+}
