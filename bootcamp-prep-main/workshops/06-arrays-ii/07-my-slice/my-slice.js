bca4487037c3b432f31c0e771db7d9ad2bc45a21
manzeelanepali <nepalimanzeela@gmail.com>
2023-04-20 12:16:09 +0545

    test my slice

--- bootcamp-prep-main/workshops/06-arrays-ii/07-my-slice/my-slice.js
+++ bootcamp-prep-main/workshops/06-arrays-ii/07-my-slice/my-slice.js
@@ -1,1 +1,24 @@
 // YOUR CODE BELOW
+function mySlice(orginalArray, startIdx, endIdx) {
+  debugger;
+  let newArray = [];
+
+  const arrayLength = orginalArray.length;
+
+  if (startIdx === undefined) {
+    startIdx = 0;
+  } else if (startIdx < 0) {
+    startIdx = arrayLength + startIdx;
+  }
+
+  if (endIdx === undefined) {
+    endIdx = arrayLength;
+  } else if (endIdx < 0) {
+    endIdx = arrayLength + endIdx;
+  }
+
+  for (let i = startIdx; i < endIdx; i++) {
+    newArray.push(orginalArray[i]);
+  }
+  return newArray;
+}
