4e09651c70cba8a1628cc04cd37a47b20686d1d0
manzeelanepali <nepalimanzeela@gmail.com>
2023-04-24 11:45:39 +0545

    objects - test 1st - total amount spent on friday

--- bootcamp-prep-main/workshops/07-objects/01-last-friday-night/last-friday-night.js
+++ bootcamp-prep-main/workshops/07-objects/01-last-friday-night/last-friday-night.js
@@ -1,1 +1,7 @@
-// YOUR CODE BELOW
+function lastFridayNight(transactions) {
+  let totalAmount = 0;
+  for (let i = 0; i < transactions.length; i++) {
+    totalAmount = totalAmount + transactions[i].amount;
+  }
+  return totalAmount;
+}
