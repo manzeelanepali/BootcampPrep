2086b673c1825339bf71ef358e793023219a4306
manzeelanepali <nepalimanzeela@gmail.com>
2023-04-24 20:26:28 +0545

    call them all

--- bootcamp-prep-main/workshops/08-object-methods/03-call-them-all/call-them-all.js
+++ bootcamp-prep-main/workshops/08-object-methods/03-call-them-all/call-them-all.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+function callThemAll(obj, value) {
+  let results = [];
+
+  // Iterate over all properties of the object
+  for (let prop in obj) {
+    // Check if the property is a function
+    if (typeof obj[prop] === "function") {
+      // Call the function with the given value as argument
+      results.push(obj[prop](value));
+    }
+  }
+
+  return results;
+}
