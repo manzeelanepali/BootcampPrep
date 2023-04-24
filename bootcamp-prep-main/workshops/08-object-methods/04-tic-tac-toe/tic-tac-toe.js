e1f793241e2838c6dcbe2ff38a870ae9bc3f20ff
manzeelanepali <nepalimanzeela@gmail.com>
2023-04-24 20:32:57 +0545

    tic tac toe

--- bootcamp-prep-main/workshops/08-object-methods/04-tic-tac-toe/tic-tac-toe.js
+++ bootcamp-prep-main/workshops/08-object-methods/04-tic-tac-toe/tic-tac-toe.js
@@ -1,1 +1,26 @@
 // YOUR CODE BELOW
+let ticTacToe = {
+  board: [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+  ],
+
+  move: function (char, row, col) {
+    if (this.board[row][col] === null) {
+      this.board[row][col] = char;
+    }
+    console.log(this.board);
+    return this.board;
+  },
+
+  clear: function () {
+    this.board = [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null],
+    ];
+    console.log(this.board);
+    return this.board;
+  },
+};
