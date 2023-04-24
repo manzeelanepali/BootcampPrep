6d3f11447c45177c3e4198faf9919f3a32c73b01
manzeelanepali <nepalimanzeela@gmail.com>
2023-04-24 12:57:51 +0545

    greetings test

--- bootcamp-prep-main/workshops/08-object-methods/02-say-your-name/say-your-name.js
+++ bootcamp-prep-main/workshops/08-object-methods/02-say-your-name/say-your-name.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+const me2 = {
+  name: "Tarana",
+  getGreeting(other) {
+    return `Hi ${other.name}, my name is ${this.name}.`;
+  },
+};
+
+const you = {
+  name: "Alyssa",
+};
