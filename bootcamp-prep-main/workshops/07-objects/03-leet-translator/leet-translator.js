46b947a138aa4314f1a7f604b51de1be88f85d6d
manzeelanepali <nepalimanzeela@gmail.com>
2023-04-24 12:20:19 +0545

    conversion of the plain string to leet character

--- bootcamp-prep-main/workshops/07-objects/03-leet-translator/leet-translator.js
+++ bootcamp-prep-main/workshops/07-objects/03-leet-translator/leet-translator.js
@@ -1,4 +1,133 @@
-let letters = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ];
-let leetChars = ['@', '8', '(', '|)', '3', 'ph', 'g', '#','l', '_|', '|<', '1', "|'|'|", '/\/', '0', '|D', '(,)', '|2', '5', '+', '|_|', '|/', "|/|/'",'><', 'j', '2'];
+let letters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+let leetChars = [
+  "@",
+  "8",
+  "(",
+  "|)",
+  "3",
+  "ph",
+  "g",
+  "#",
+  "l",
+  "_|",
+  "|<",
+  "1",
+  "|'|'|",
+  "//",
+  "0",
+  "|D",
+  "(,)",
+  "|2",
+  "5",
+  "+",
+  "|_|",
+  "|/",
+  "|/|/'",
+  "><",
+  "j",
+  "2",
+];
 
 // YOUR CODE BELOW
+function leetTranslator(str) {
+  let letters = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  let leetChars = [
+    "@",
+    "8",
+    "(",
+    "|)",
+    "3",
+    "ph",
+    "g",
+    "#",
+    "l",
+    "_|",
+    "|<",
+    "1",
+    "|'|'|",
+    "//",
+    "0",
+    "|D",
+    "(,)",
+    "|2",
+    "5",
+    "+",
+    "|_|",
+    "|/",
+    "|/|/'",
+    "><",
+    "j",
+    "2",
+  ];
+
+  let leetCodex = {};
+  for (let i = 0; i < letters.length; i++) {
+    leetCodex[letters[i]] = leetChars[i];
+  }
+
+  let leetString = "";
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i].toLowerCase();
+    if (leetCodex[char]) {
+      leetString += leetCodex[char];
+    } else {
+      leetString += char;
+    }
+  }
+
+  return leetString;
+}
