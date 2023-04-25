dcf3faeec432aae3de505288493cd8222097c1a3
manzeelanepali <nepalimanzeela@gmail.com>
2023-04-25 13:25:06 +0545

    cloning

--- bootcamp-prep-main/workshops/09-pbv-pbr/03-clone-machine/clone-machine.js
+++ bootcamp-prep-main/workshops/09-pbv-pbr/03-clone-machine/clone-machine.js
@@ -1,8 +1,21 @@
 // YOUR CODE BELOW
 function cloneMachine(animal) {
+  debugger;
+  //  The Object.assign method is used to create a shallow copy of the animal object and assign it to a clone variable.
   let clone = Object.assign({}, animal);
+  //   The name of the clone is set to the name of the original animal, concatenated with the word 'Clone'.
   clone.name = animal.name + "Clone";
+  //   The offspring property of the clone is set to an empty array.
   clone.offspring = [];
   animal.offspring.push(clone.name);
+  //   The name of the clone is pushed to the offspring array of the original animal.
   return clone;
 }
+
+console.log(
+  cloneMachine({
+    name: "Dolly",
+    species: "sheep",
+    offspring: [],
+  })
+);
