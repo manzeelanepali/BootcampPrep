42cbb06ed96b5f2c725502d7c0f67b9335de5c5d
manzeelanepali <nepalimanzeela@gmail.com>
2023-04-25 12:29:19 +0545

    finding odd number

--- bootcamp-prep-main/workshops/09-pbv-pbr/01-very-odd/very-odd.js
+++ bootcamp-prep-main/workshops/09-pbv-pbr/01-very-odd/very-odd.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+function veryOdd(array) {
+  let oddNum = [];
+
+  for (let i = 0; i < array.length; i++) {
+    let currVal = array[i];
+    if (currVal % 2 !== 0) {
+      oddNum.push(currVal);
+    }
+  }
+
+  return oddNum;
+}
+
+console.log(veryOdd([1, 2, 3, 4, 5, 6, 7, 8]));
