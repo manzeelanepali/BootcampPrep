4e2976ed96049acbf3e065c0812f83106ae68135
manzeelanepali <nepalimanzeela@gmail.com>
2023-04-25 12:44:13 +0545

    replacing the even number with normie and returning the array without mutating

--- bootcamp-prep-main/workshops/09-pbv-pbr/02-very-odd-mutant/very-odd-mutant.js
+++ bootcamp-prep-main/workshops/09-pbv-pbr/02-very-odd-mutant/very-odd-mutant.js
@@ -1,1 +1,25 @@
 // YOUR CODE BELOW
+// function veryOddMutant (array ){
+//     let finalResult = []
+// for (let i=0;i<array.length; i++){
+//     let currVal = array[i]
+//     if (currVal%2===0){
+
+//     }
+// }
+
+// return finalResult;
+// }
+// console.log(veryOddMutant([1,2,3,4,5,6,7,8,9,10]))
+function veryOddMutant(arr) {
+  debugger;
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      arr[i] = "normie";
+      count++;
+    }
+  }
+  return count;
+}
+console.log(veryOddMutant([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
