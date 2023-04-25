4781569db0e9eeff5e962da06be1797d305e5118
manzeelanepali <nepalimanzeela@gmail.com>
2023-04-25 12:14:56 +0545

    npm install in ex-9

--- bootcamp-prep-main/workshops/08-object-methods/05-taco-cat-inc/taco-cat-inc.js
+++ bootcamp-prep-main/workshops/08-object-methods/05-taco-cat-inc/taco-cat-inc.js
@@ -1,25 +1,25 @@
 let tacoCatInc = {
   gourmetShell: {
-    'hard treat shell': {cost: 2, quantity: 100},
-    'soft treat shell': {cost: 1.5, quantity: 100}
+    "hard treat shell": { cost: 2, quantity: 100 },
+    "soft treat shell": { cost: 1.5, quantity: 100 },
   },
 
   gourmetFishFilling: {
-    'salmon': {cost: 5, quantity: 100},
-    'tuna': {cost: 5.5, quantity: 100},
-    'sardines': {cost: 1.5, quantity: 100}
+    salmon: { cost: 5, quantity: 100 },
+    tuna: { cost: 5.5, quantity: 100 },
+    sardines: { cost: 1.5, quantity: 100 },
   },
 
   gourmetVeggie: {
-    'cat grass': {cost: 1, quantity: 100}
+    "cat grass": { cost: 1, quantity: 100 },
   },
 
   gourmetSeasoning: {
-    'cat nip': {cost: 0.5, quantity: 100},
-    'treat dust': {cost: 0.1, quantity: 100}
+    "cat nip": { cost: 0.5, quantity: 100 },
+    "treat dust": { cost: 0.1, quantity: 100 },
   },
 
-  cash: 0
+  cash: 0,
 };
 
 // YOUR CODE BELOW
